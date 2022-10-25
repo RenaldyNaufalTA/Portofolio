5d9259207409dd338c8ed0461b4172cda8e1e240
Renaldy Naufal <aldyreyn5@gmail.com>
2022-10-25 11:19:16 +0700

    edit

--- resources/views/layouts/admin/script/js.blade.php
+++ resources/views/layouts/admin/script/js.blade.php
@@ -1,12 +1,12 @@
 <!-- Bootstrap core JavaScript-->
-<script src="./template/vendor/jquery/jquery.min.js"></script>
-<script src="./template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../template/vendor/jquery/jquery.min.js"></script>
+<script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="./template/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="../template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="./template/js/sb-admin-2.min.js"></script>
+<script src="../template/js/sb-admin-2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
